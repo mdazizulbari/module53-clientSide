54911b3fe084d886e2975d8a5ca0a6fffe33373b
smabari <mdazizulbarishafin@gmail.com>
2025-05-20 23:12:02 +0600

    showed in the client server and recieved from there too

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLoaderData } from "react-router";
 
 const Posts = () => {
-  const posts = useLoaderData();
+  const initialPosts = useLoaderData();
+  const [posts, setPosts] = useState(initialPosts);
   console.log(posts);
+
+  const handleAddPost = (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    const post = { title, body };
+    console.log(post);
+    // create post in the server
+    fetch("http://localhost:5000/posts", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(post),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data after post method", data);
+        const newPosts = [...posts, data];
+        setPosts(newPosts);
+        event.target.reset();
+      });
+  };
 
   return (
     <div>
+      <div className="">
+        <form onSubmit={handleAddPost} action="">
+          <input type="text" name="title" id="" />
+          <br />
+          <input type="text" name="body" id="" />
+          <br />
+          <input type="submit" value="Add Post" />
+        </form>
+      </div>
+
       <h2>All Posts here: {posts.length}</h2>
       {posts.map((posts) => (
         <li key={posts.id}>
